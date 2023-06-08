3475b8865f4025276ef9bd163fc4adb8f846f4d8
mordecai <mordecai.mengesteab@gmail.com>
2023-06-08 18:40:28 -0400

    Finished product component. Adding cart addition functionality

--- src/products.js
+++ src/products.js
@@ -1,1 +1,19 @@
-export const PRODUCTS = [{ id: 1, productName: "iPhone", price: 999.99 }];
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.png";
+import product5 from "./assets/5.png";
+import product6 from "./assets/6.webp";
+import product7 from "./assets/7.webp";
+import product8 from "./assets/8.webp";
+
+export const PRODUCTS = [
+  { id: 1, productName: "iPhone", price: 999.99, productImg: product1 },
+  { id: 2, productName: "MacBook", price: 1299.99, productImg: product2 },
+  { id: 3, productName: "Camera", price: 499.99, productImg: product3 },
+  { id: 4, productName: "Hoodie", price: 49.99, productImg: product4 },
+  { id: 5, productName: "Lights", price: 29.99, productImg: product5 },
+  { id: 6, productName: "Sweater", price: 39.99, productImg: product6 },
+  { id: 7, productName: "Shirt", price: 29.99, productImg: product7 },
+  { id: 8, productName: "Hat", price: 9.99, productImg: product8 },
+];
